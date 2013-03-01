98c24dcb9bc9eb8b1ff6ec4ef6740505a11fbfcc
fjhickernell <hickernell@iit.edu>
2013-03-01 12:56:43 -0600

    revised

--- TestMeanMCTrafficModel.m
+++ TestMeanMCTrafficModel.m
@@ -1,10 +1,10 @@
-
+% Testing meanMC on the Nagel-Schreckenberg Traffic Model
 param.timebudget=20;
 param.nbudget=1e6;
 param.n0=70;
 param.fudge=1.1;
-param.alpha=0.01;
-param.tol=1e-2;
+param.alpha=0.01; %uncertainty
+param.tol=3e-3; %absolute error tolerance
 %param.nmax=1e4;
 param.npcmax=90;
 [mu,param]=meanMC(@Ytrafficmodel,param)
