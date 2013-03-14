6dc04a21755aec442596e9542fac72d3e3691068
Lan Jiang <tojianglan@gmail.com>
2013-03-14 16:04:34 -0500

    minor change

--- TestMeanMCTrafficModel.m
+++ TestMeanMCTrafficModel.m
@@ -1,11 +1,11 @@
-% this is the drive file to test meanMC package,
-% using the Nagel-Schreckenberg traffic model 
+% this is the drive file to test meanMC package on...
+% the Nagel-Schreckenberg traffic model 
 param.timebudget=10;% given time budget 
 param.nbudget=1e6;% given sample budget
 param.n0=70;% given intitial sample szie to estimate sigma
-param.fudge=1.1;%given fudge factor
+param.fudge=1.1;% given fudge factor
 param.alpha=0.01;% given uncertainty
-param.tol=1e-2;%given error tolerance
+param.tol=1e-2;% given error tolerance
 param.npcmax=90;% given piecewise maximum to calculate the mu
 [mu,param]=meanMC(@Ytrafficmodel,param)
 
--- meanMCparam.m
+++ meanMCparam.m
@@ -1,58 +1,58 @@
 function [param]=meanMCparam(param)
 param.exit=0;    %success!
-tolDefault=1e-3;      %absolute error tolerance
-nmaxDefault=1e9;       %maximum number of scalar sample points
-npcmaxDefault=1e5;       %maximum size of a vector of samples
-n0Default=30;      %initial number of samples
-n0MinDefault=30;        %minimum allowed initial number of samples
-alphaDefault=0.01;      %uncertainty
-fudgeDefault=1.1;       %fudge factor for error estimate
-timebudgetDefault=30; % default time budget
-nbudgetDefault=1e9; % default N budget
-%Error tolerance
+tolDefault=1e-3; %absolute error tolerance
+nmaxDefault=1e9; %maximum number of scalar sample points
+npcmaxDefault=1e5;%maximum size of a vector of samples
+n0Default=30;     %initial number of samples
+n0MinDefault=30;  %minimum allowed initial number of samples
+alphaDefault=0.01;%uncertainty
+fudgeDefault=1.1; %fudge factor for error estimate
+timebudgetDefault=30;%default time budget
+nbudgetDefault=1e9; %default N budget
+% Error tolerance
 if isfield(param,'tol');
     param.tol=abs(param.tol);
 else
     param.tol=tolDefault; 
 end
-% time budget
+% Time budget
 if isfield(param,'timebudget')
     param.timebudget=abs(param.timebudget);
 else
     param.timebudget=timebudgetDefault;
 end
-%sample budget
+% Sample budget
 if isfield(param,'nbudget')
     param.nbudget=abs(param.nbudget);
 else
     param.nbudget=nbudgetDefault;
 end
 
-% max N
+% Max N
 if isfield(param,'nmax');
     param.nmax=abs(param.nmax);
 else
     param.nmax=nmaxDefault;
 end
-%Maxinum number of scalar values of x per vector
+% Maxinum number of scalar values of x per vector
 if isfield(param,'npcmax');
     param.npcmax=abs(param.npcmax);
 else
     param.npcmax=npcmaxDefault;
 end
-%Initial sample size
+% Initial sample size
 if isfield(param,'n0');
     param.n0=max(param.n0,n0MinDefault);
 else
     param.n0=n0Default; 
 end
-%Fudge factor to multiply error estimate by
+% Fudge factor to multiply error estimate by
 if isfield(param,'fudge');
     param.fudge=max(abs(param.fudge));
 else
     param.fudge=fudgeDefault; 
 end
-%Uncertainty
+% Uncertainty
 if isfield(param,'alpha');
     param.alpha=min(max(abs(param.alpha),0),1);
 else
